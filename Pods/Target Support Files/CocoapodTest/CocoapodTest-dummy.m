a80fdfe5382758856a73db7cad9615255d0ae905
李大米 <758451690@qq.com>
2016-05-25 18:56:09 +0800

    pod update 用spec

---
+++ Pods/Target Support Files/CocoapodTest/CocoapodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CocoapodTest : NSObject
+@end
+@implementation PodsDummy_CocoapodTest
+@end
---
+++ Pods/Target Support Files/CocoapodTest/CocoapodTest-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+FOUNDATION_EXPORT double CocoapodTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoapodTestVersionString[];
+
